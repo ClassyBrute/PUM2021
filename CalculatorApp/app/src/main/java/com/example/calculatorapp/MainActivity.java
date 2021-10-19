7b9bed4fb41039a6474e0a57773a456831c03475
ClassyBrute <315696@uwr.edu.pl>
2021-10-19 19:35:19 +0200

    kalkulator

--- CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.calculatorapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView showResult;
+    private int result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showResult = findViewById(R.id.show_result);
+
+        if (savedInstanceState != null)
+            result = savedInstanceState.getInt("counter_state");
+
+        if (showResult != null)
+            showResult.setText(Integer.toString(result));
+    }
+
+
+    public void enterDigit(int digit){
+
+
+    }
+
+
+    public void enterOperator(){
+
+
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("counter_state", result);
     }
 }
