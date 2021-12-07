cbc4646087654b0d05decd0bdf0f7b4753d5913f
ClassyBrute <315696@uwr.edu.pl>
2021-12-07 17:45:07 +0100

    no teraz juz wszystko dziala ladnie

--- StudentCrimeApp/app/src/main/java/com/example/studentcrimeapp/CrimeAdapter.java
+++ StudentCrimeApp/app/src/main/java/com/example/studentcrimeapp/CrimeAdapter.java
@@ -9,7 +9,6 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.ViewPager;
 
 import java.util.LinkedList;
 
